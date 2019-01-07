e026b969fd3ddf514abad70e444fb891b76160f6
CYTan <tcy.cloud@gmail.com>
2019-01-07 10:43:10 +0800

    bug fix

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -12,7 +12,7 @@ echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 # Code to set up the parks development project.
 
 # To be Implemented by Student
-oc policy add-role-to-user edit system:serviceaccount:tcy-jenkins:jenkins -n tcy-parks-dev
+oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-parks-dev
 oc new-app -f ../templates/mongodb-persistent-template.json -n ${GUID}-parks-dev \
 --param MONGODB_USER="mongodb" \
 --param MONGODB_PASSWORD="mongodb" \
@@ -28,10 +28,12 @@ oc create configmap mlbparks-config -n ${GUID}-parks-dev \
 --from-literal="DB_USERNAME=mongodb" \
 --from-literal="DB_PASSWORD=mongodb" \
 --from-literal="DB_NAME=parks" \
---from-literal="APPNAME=National Parks (Dev)"
+--from-literal="APPNAME=MLB Parks (Dev)"
 oc set env dc/mlbparks --from=configmap/mlbparks-config -n ${GUID}-parks-dev
 oc set triggers dc/mlbparks --remove-all -n ${GUID}-parks-dev
 oc expose dc mlbparks --port 8080 -n ${GUID}-parks-dev
-oc expose svc mlbparks -n ${GUID}-parks-dev -l type=parksmap-backend
+oc expose svc mlbparks -n ${GUID}-parks-dev
 oc set probe dc/mlbparks --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-dev
 oc set probe dc/mlbparks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
+
+# 
