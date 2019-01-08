51423313628d3a65629279abdbd6099837214b32
CYTan <tcy.cloud@gmail.com>
2019-01-08 16:30:19 +0800

    adding 2 more micro services in dev

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -36,4 +36,35 @@ oc expose svc mlbparks -n ${GUID}-parks-dev
 oc set probe dc/mlbparks --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-dev
 oc set probe dc/mlbparks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
 
-# 
+# National Parks
+oc create configmap nationalparks-config -n ${GUID}-parks-dev \
+--from-literal="DB_HOST=mongodb" \
+--from-literal="DB_PORT=27017" \
+--from-literal="DB_USERNAME=mongodb" \
+--from-literal="DB_PASSWORD=mongodb" \
+--from-literal="DB_NAME=parks" \
+--from-literal="APPNAME=National Parks (Dev)"
+
+oc new-build --binary=true --name="nationalparks" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-dev
+oc new-app ${GUID}-parks-dev/nationalparks:0.0-0 --name=nationalparks -l type=parksmap-backend --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
+
+oc set env dc/nationalparks --from=configmap/nationalparks-config -n ${GUID}-parks-dev
+oc set triggers dc/nationalparks --remove-all -n ${GUID}-parks-dev
+oc expose dc nationalparks --port 8080 -n ${GUID}-parks-dev
+oc expose svc nationalparks -l type=parksmap-backend -n ${GUID}-parks-dev
+oc set probe dc/nationalparks --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-dev
+oc set probe dc/nationalparks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
+
+# ParksMap
+oc create configmap parksmap-config -n ${GUID}-parks-dev \
+--from-literal="APPNAME=ParksMap (Dev)"
+
+oc new-build --binary=true --name="parksmap" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-dev
+oc new-app ${GUID}-parks-dev/parksmap:0.0-0 --name=parksmap --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
+
+oc set env dc/parksmap --from=configmap/parksmap-config -n ${GUID}-parks-dev
+oc set triggers dc/parksmap --remove-all -n ${GUID}-parks-dev
+oc expose dc parksmap --port 8080 -n ${GUID}-parks-dev
+oc expose svc parksmap -n ${GUID}-parks-dev
+oc set probe dc/parksmap --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-dev
+oc set probe dc/parksmap --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
