2d6c88cae626724bbb27ea6d547e1c2d765d485c
CYTan <tcy.cloud@gmail.com>
2019-01-09 21:54:21 +0800

    reset porudction scripts

--- Infrastructure/bin/reset_prod_blue.sh
+++ Infrastructure/bin/reset_prod_blue.sh
@@ -18,48 +18,47 @@ echo "Resetting Parks Production Environment in project ${GUID}-parks-prod to Gr
 # rollout followed by a Green rollout.
 
 # To be Implemented by Student
-
 printf "\n"
-destApp="mlbparks-green"
+destApp="mlbparks-blue"
 
 activeApp="$(oc get route mlbparks -n ${GUID}-parks-prod -o jsonpath='{ .spec.to.name }')"
 echo ${activeApp}
 
-if [ $activeApp == 'mlbparks-blue' ]; then
-    echo "Reseting mlbparks to Green"
+if [ $activeApp == 'mlbparks-green' ]; then
+    echo "Reseting mlbparks to Blue"
     oc delete svc ${activeApp} -n ${GUID}-parks-prod
     oc expose dc/${destApp} -l type=parksmap-backend --port 8080 -n ${GUID}-parks-prod
     oc patch route mlbparks -n ${GUID}-parks-prod -p '{"spec":{"to":{"name":"'$destApp'"}}}'
 else
-    echo "mlbparks is pointing Green already"
+    echo "mlbparks is pointing Blue already"
 fi
 
 printf "\n"
-destApp="nationalparks-green"
+destApp="nationalparks-blue"
 
 activeApp="$(oc get route nationalparks -n ${GUID}-parks-prod -o jsonpath='{ .spec.to.name }')"
 echo ${activeApp}
 
-if [ $activeApp == 'nationalparks-blue' ]; then
-    echo "Reseting nationalparks to Green"
+if [ $activeApp == 'nationalparks-green' ]; then
+    echo "Reseting nationalparks to Blue"
     oc delete svc ${activeApp} -n ${GUID}-parks-prod
     oc expose dc/${destApp} -l type=parksmap-backend --port 8080 -n ${GUID}-parks-prod
     oc patch route nationalparks -n ${GUID}-parks-prod -p '{"spec":{"to":{"name":"'$destApp'"}}}'
 else
-    echo "nationalparks is pointing Green already"
+    echo "nationalparks is pointing Blue already"
 fi
 
 printf "\n"
-destApp="parksmap-green"
+destApp="parksmap-blue"
 
 activeApp="$(oc get route parksmap -n ${GUID}-parks-prod -o jsonpath='{ .spec.to.name }')"
 echo ${activeApp}
 
-if [ $activeApp == 'parksmap-blue' ]; then
-    echo "Reseting parksmap to Green"
+if [ $activeApp == 'parksmap-green' ]; then
+    echo "Reseting parksmap to Blue"
     oc delete svc ${activeApp} -n ${GUID}-parks-prod
     oc expose dc/${destApp} --port 8080 -n ${GUID}-parks-prod
     oc patch route parksmap -n ${GUID}-parks-prod -p '{"spec":{"to":{"name":"'$destApp'"}}}'
 else
-    echo "parksmap is pointing Green already"
+    echo "parksmap is pointing Blue already"
 fi
